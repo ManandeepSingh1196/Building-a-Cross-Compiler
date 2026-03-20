4372b16dfdae5d84458887f17cabbb75d7fc2645
ManandeepSingh1196 <manan801p@gmail.com>
2026-03-20 19:39:40 +0530

    Completed.

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 TARGET="x86_64-elf"
 PREFIX="$HOME/opt/cross"
 SRCDIR="$HOME/src/cross-toolchain"
+$PKG_MGR="pacman"
 
 BINUTILS_VER="${BINUTILS_VER:2.43}"
 GCC_VER="${GCC_VER:-14.2.0}"
@@ -35,8 +36,9 @@ check_dependencies ()
     done
 
     [[ ${#missing[0]} - gt 0 ]] && \
-        die "Missing: ${missing[*]}\nRun: sudo pacman -S base-devel gmp libmpc mpfr"
-success "All dependencies present."
+        die "Missing: ${missing[*]}\nRun: sudo $PKG_MGR -S base-devel gmp mpc libmpc mpfr"
+    
+    success "All dependencies present."
 }
 
 # Download Sources
@@ -61,3 +63,85 @@ download_sources()
 
     success "Sources ready."
 }
+
+# BUILDING BINUTILS
+build_binutils()
+{
+    info "Building binutils ${BINUTILS_VER}.. "
+    cd "${SRCDIR}"
+    
+    sudo rm -rf build-binutils 
+    mkdir build-binutils && cd build-binutils
+
+    ../binutils-${BINUTILS_VER}/configure \
+        --prefix=$PREFIX    \
+        --target=$TARGET    \
+        --with-sysroot      \
+        --disable-nls       \
+        --disable-werror    \
+        --quiet
+
+    make -j$(nproc)
+    make install
+
+    success "binutils built."
+}
+
+# BUILDING GCC
+build_gcc() 
+{
+    info "Building GCC ${GCC_VER}. This might take some time.. "
+    export PATH="$PREFIX/bin:$PATH"
+    cd "$SRCDIR"
+
+    sudo rm -rf build-gcc
+    mkdir build-gcc && cd build-gcc
+
+    ../gcc-${GCC_VER}/configure     \
+        --prefix=$PREFIX            \
+        --target=$TARGET            \
+        --disable-nls               \
+        --without-headers           \
+        --enable-languages=c,c++    \
+        --quiet
+
+    make -j$(nproc) all-gcc
+    make -j$(nproc) all-target-libgcc
+
+    make install-gcc
+    make install-target-libgcc
+
+    success "GCC built."
+}
+
+# VERIFYING
+verify() 
+{
+    info "Verifying..."
+    export PATH="$PREFIX/bin:$PATH"
+
+    "${TARGET}-gcc" --version &>/dev/null || die "${TARGET}-gcc not found."
+    "${TARGET}-ld"  --version &>/dev/null || die "${TARGET}-ld not found."
+
+    success "${TARGET}-gcc: $("${TARGET}-gcc" --version | head -1)"
+    success "${TARGET}-ld:  $("${TARGET}-ld"  --version | head -1)"
+
+    echo ""
+    echo "Add to your .bashrc / .zshrc:"
+    echo "  export PATH=\"$PREFIX/bin:\$PATH\""
+}
+
+# MAIN 
+main ()
+{
+    info "Build script for a x86_64-elf cross compiler."
+    info "binutils ${BINUTILS_VER} | GCC ${GCC_VER} | prefix:$PREFIX"
+
+    check_deps
+    download_sources
+    build_binutils
+    build_gcc
+    verify
+}
+
+main "$@"
